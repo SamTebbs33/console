1639c43329a87cc7fcecf9d0f3dccc9e2175e807
Sam Tebbs <samuel.tebbs@yahoo.com>
2019-07-23 00:14:41 +0100

    Abstracting away the PPU and VPU

--- main.zig
+++ main.zig
@@ -19,14 +19,23 @@ var spr_rom = [_]u8{0} ** SPR_ROM_SIZE;
 // VRAM
 const VRAM_SIZE = 8 * 1024;
 var vram = [_]u8{0} ** VRAM_SIZE;
-// Duplicate of VRAM
-var vram2 = [_]u8{0} ** VRAM_SIZE;
 // CPU firmware ROM
 const CPU_ROM_SIZE = 32 * 1024;
 var cpu_rom = @embedFile("cpu.bin");
 // PPU firmware ROM
 const PPU_ROM_SIZE = 8 * 1024;
 var ppu_rom = @embedFile("ppu.bin");
+
+const TILE_TABLE_ADDR = 0;
+const TILE_TABLE_SIZE = NUM_TILES_X * NUM_TILES_Y * TILE_ENTRY_SIZE;
+const NUM_TILE_TABLES = 2;
+const TILE_ENTRY_SIZE = 3;
+const SPRITE_ADDR = 0;
+const SPRITE_SIZE = SPRITE_WIDTH * SPRITE_HEIGHT * PIXEL_SIZE;
+const PIXEL_SIZE = 1;
+const PALETTE_ADDR = TILE_TABLE_ADDR + TILE_TABLE_SIZE * NUM_TILE_TABLES;
+const PALETTE_SIZE = 16 * COLOUR_SIZE;
+const COLOUR_SIZE = 1;
 
 comptime {
     std.debug.assert(cpu_rom.len <= CPU_ROM_SIZE);
@@ -35,31 +44,18 @@ comptime {
 
 // Processors
 var cpu = initZ80(memRead, memWrite, ioRead, ioWrite);
-var ppu = initZ80(memRead, memWrite, ioRead, ioWrite);
 
-// The nano seconds per CPU clock
-const CPU_NANOS = 100;
-// The nano seconds per PPU clock
-const PPU_NANOS = 100;
-// The nano seconds per tick
-const TICK_NANOS = 100;
-// The nano seconds taken to draw a scanline
-const SCANLINE_DRAW_NANOS = 75000;
-// The nano seconds per hblank period
-const HBLANK_NANOS = 25000;
-// The nano seconds for the drawing each scanline and each hblank period
-const DRAW_NANOS = 25000000;
-// The nano seconds taken by vblank
-const VBLANK_NANOS = 15000000;
-const FRAME_NANOS = DRAW_NANOS + VBLANK_NANOS;
-var cpu_nanos: u32 = 0;
-var ppu_nanos: u32 = 0;
-var draw_nanos: u32 = 0;
-var scanline: u32 = 0;
-const RES_X = 250;
+const RES_X = 240;
 const RES_Y = 240;
-const FINAL_SCANLINE = RES_Y;
-const PIXELS_IO_ADDR = 0;
+const SPRITE_WIDTH = 8;
+const SPRITE_HEIGHT = 8;
+const NUM_TILES_X = RES_X / SPRITE_WIDTH;
+const NUM_TILES_Y = RES_Y / SPRITE_HEIGHT;
+const FPS = 50;
+const NANOS_PER_CPU_CYCLE = 100;
+const NANOS_PER_FRAME = 1000000000 / FPS;
+const CPU_CYCLES_PER_FRAME = NANOS_PER_FRAME / NANOS_PER_CPU_CYCLE;
+var nanos: u64 = 0;
 
 fn initZ80(mem_read: z80.Z80DataIn, mem_write: z80.Z80DataOut, io_read: z80.Z80DataIn, io_write: z80.Z80DataOut) z80.Z80Context {
     var ctx: z80.Z80Context = undefined;
@@ -78,7 +74,6 @@ fn getMappedMem(is_cpu: bool, address: u16) *u8 {
             else => unreachable
         };
     } else {
-        //std.debug.warn("PPU addr: {}, {}\n", address, ppu.nmi_req);
         return switch (address) {
             0 ... PPU_ROM_SIZE - 1 => &ppu_rom[address],
             PPU_ROM_SIZE ... PPU_ROM_SIZE + SPR_ROM_SIZE - 1 => &spr_rom[address - PPU_ROM_SIZE],
@@ -116,31 +111,68 @@ export fn ioWrite(param: c_int, address: c_ushort, byte: u8) void {
     getMappedIo(param == 1, address).* = byte;
 }
 
-fn tick_processor(processor: *z80.Z80Context, curr_nanos: *u32, max_nanos: u32) void {
-        if (curr_nanos.* >= max_nanos) {
-            z80.Z80Execute(processor);
-            curr_nanos.* = 0;
-        } else {
-            curr_nanos.* += TICK_NANOS;
+fn drawSprite(renderer: *sdl.SDL_Renderer, sprite_addr: u16, palette_addr: u16, x: u8, y: u8) void {
+    var sprite_x: u8 = 0;
+    while (sprite_x < SPRITE_WIDTH) : (sprite_x += 1) {
+        var sprite_y: u8 = 0;
+        while (sprite_y < SPRITE_HEIGHT) : (sprite_y += 1) {
+            const pixel_num = sprite_x * SPRITE_HEIGHT + sprite_y;
+            const pixel_addr = sprite_addr + pixel_num / 2;
+            const pixel: u8 = spr_rom[pixel_addr];
+            const colour: u8 = vram[palette_addr + pixel];
+            const red = if ((colour & 0b111) == 0b111) u8(255) else 0;
+            const green = if ((colour & 0b111000) == 0b111000) u8(255) else 0;
+            const blue = if ((colour & 0b11000000) == 0b11000000) u8(255) else 0;
+            var ignored = sdl.SDL_SetRenderDrawColor(renderer, red, green, blue, 255);
+            ignored = sdl.SDL_RenderDrawPoint(renderer, x * SPRITE_WIDTH + sprite_x, y * SPRITE_HEIGHT + sprite_y);
         }
+    }
 }
 
 fn draw(renderer: *sdl.SDL_Renderer) void {
-    // Draw each pixel on the current scanline
-    var x: c_int = 0;
-    while (x < RES_X) : (x += 1) {
-        var ignored = sdl.SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-        ignored = sdl.SDL_RenderDrawPoint(renderer, x, @intCast(c_int, scanline));
+    // Clear the screen first
+    var ignored = sdl.SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+    ignored = sdl.SDL_RenderClear(renderer);
+    // Draw the background tiles
+    var tile_x: u8 = 0;
+    const tile_table = 0;
+    const tile_table_addr = TILE_TABLE_ADDR + tile_table * TILE_TABLE_SIZE;
+    while (tile_x < NUM_TILES_X) : (tile_x += 1) {
+        var tile_y: u8 = 0;
+        while (tile_y < NUM_TILES_Y) : (tile_y += 1) {
+            const tile_entry_addr = tile_table_addr + tile_x * NUM_TILES_Y + tile_y;
+            const tile_entry: u16 = vram[tile_entry_addr] | u16(vram[tile_entry_addr + 1]) << 8;
+            const enabled = ((tile_entry >> 12) & 0b1) == 1;
+            if (enabled) {
+                const tile_entry_x = tile_entry & 0b11111;
+                const tile_entry_y = (tile_entry >> 5) & 0b11111;
+                const palette = (tile_entry >> 10) & 0b11;
+                const sprite_num = vram[tile_entry_addr + 2];
+                const sprite_addr = SPRITE_ADDR + sprite_num * SPRITE_SIZE;
+                const palette_addr = PALETTE_ADDR + palette * PALETTE_SIZE;
+                drawSprite(renderer, sprite_addr, palette_addr, tile_x * SPRITE_WIDTH, tile_y * SPRITE_HEIGHT);
+            }
+        }
     }
     sdl.SDL_RenderPresent(renderer);
 }
 
-pub fn main() !void {
-    std.debug.warn("ROM len: {}\n", ppu_rom.len);
-    for (ppu_rom) |rom_byte, i| {
-        std.debug.warn("Byte at {} is {}\n", i, rom_byte);
+fn initGraphics() void {
+    // Set sprites
+    var i: u32 = 0;
+    const sprite_num = 0;
+    while (i < SPRITE_SIZE) : (i += 1) {
+        spr_rom[SPRITE_ADDR + sprite_num * SPRITE_SIZE + i] = 0;
     }
-    std.debug.warn("Byte at 0x66 {}\n", ppu_rom[0x66]);
+    // Set palettes
+    vram[PALETTE_ADDR] = 0b11111111;
+    // Set tile table
+    vram[TILE_TABLE_ADDR] = 0b00000 | 0b000 << 5;
+    vram[TILE_TABLE_ADDR + 1] = 0b00 | 0b00 << 2 | 0b1 << 4 | 0b000 << 5 ;
+    vram[TILE_TABLE_ADDR + 2] = 0b00000000;
+}
+
+pub fn main() !void {
     if (sdl.SDL_Init(sdl.SDL_INIT_VIDEO) != 0) {
         return ConsoleError.SdlInit;
     }
@@ -157,36 +189,21 @@ pub fn main() !void {
     cpu.memParam = 1;
     cpu.ioParam = 1;
     z80.Z80RESET(&cpu);
-    ppu.memParam = 0;
-    ppu.ioParam = 0;
-    z80.Z80RESET(&ppu);
+
+    initGraphics();
 
     while (true) {
-        tick_processor(&cpu, &cpu_nanos, CPU_NANOS);
-        tick_processor(&ppu, &ppu_nanos, PPU_NANOS);
-        if (draw_nanos == (SCANLINE_DRAW_NANOS * (scanline + 1)) + (HBLANK_NANOS * scanline)) {
-            // If we have reached the current scanline's hblank
-            draw(renderer);
-            scanline += 1;
-            //std.debug.warn("Going NMI on ppu {}\n", ppu.PC);
-            z80.Z80NMI(&ppu);
-            draw_nanos += TICK_NANOS;
-        } else if (draw_nanos == (SCANLINE_DRAW_NANOS * (scanline + 1)) + (HBLANK_NANOS * (scanline + 1))) {
-            // We have reached the end of current scanline's hblank
-            if (scanline == FINAL_SCANLINE) {
-                // We've reached VBLANK
-                scanline = 0;
-                //std.debug.warn("Going NMI on ppu {}\n", ppu.PC);
-                z80.Z80NMI(&ppu);
-            }
-            draw_nanos += TICK_NANOS;
-        } else if (draw_nanos == FRAME_NANOS) {
-            // We've reached the end of the entire frame
-            scanline = 0;
-            draw_nanos = 0;
-        } else {
-            draw_nanos += TICK_NANOS;
+        var event: sdl.SDL_Event = undefined;
+        if (SDL_PollEvent(&event) != 0) {
+            if (event.@"type" == sdl.SDL_QUIT)
+                return;
         }
-        std.time.sleep(TICK_NANOS);
+        const ignored2 = z80.Z80ExecuteTStates(&cpu, CPU_CYCLES_PER_FRAME);
+        if (nanos == NANOS_PER_FRAME) {
+            draw(renderer);
+            nanos = 0;
+        }
+        nanos += NANOS_PER_CPU_CYCLE;
+        std.time.sleep(NANOS_PER_CPU_CYCLE);
     }
 }
