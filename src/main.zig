0ba923884f1b9bcaad8f84d7a769f94ca9e9998a
Sam Tebbs <samuel.tebbs@yahoo.com>
2019-08-14 22:57:09 +0100

    Scale renderer by 3

--- src/main.zig
+++ src/main.zig
@@ -292,10 +292,12 @@ pub fn main() !void {
     if (!(sdl.SDL_SetHintWithPriority(sdl.SDL_HINT_NO_SIGNAL_HANDLERS, c"1", sdl.SDL_HintPriority.SDL_HINT_OVERRIDE) != sdl.SDL_bool.SDL_FALSE)) {
         return ConsoleError.SdlInit;
     }
-    const screen = sdl.SDL_CreateWindow(c"Console", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, types.RES_X, types.RES_Y, sdl.SDL_WINDOW_RESIZABLE);
+    const scale_factor = 3;
+    const screen = sdl.SDL_CreateWindow(c"Console", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, types.RES_X * scale_factor, types.RES_Y * scale_factor, sdl.SDL_WINDOW_RESIZABLE);
     const renderer = sdl.SDL_CreateRenderer(screen, -1, 0) orelse return ConsoleError.SdlInit;
+    var ignored = sdl.SDL_RenderSetScale(renderer, f32(scale_factor), f32(scale_factor));
 
-    var ignored = sdl.SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+    ignored = sdl.SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
     ignored = sdl.SDL_RenderClear(renderer);
     sdl.SDL_RenderPresent(renderer);
 
